2934ea1d5ef50b32c8d084ff81401d39670860ef
Zununova <121685002+Zununova@users.noreply.github.com>
2023-08-21 08:10:37 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,19 +69,4 @@ dependencies {
 
     // AsyncImage
     implementation("io.coil-kt:coil-compose:2.4.0")
-
-
-
-
-    // Hilt navigation
-    implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // OkHttp
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.6")
-
 }
